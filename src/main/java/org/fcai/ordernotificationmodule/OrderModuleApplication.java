899a3c1b0a688cac50e20fca62c33a7eca9c37a1
Mohamed Hussein El-Adwy <mohamed.h.eladwy@gmail.com>
2023-12-26 18:59:33 +0200

    update some configuration files add DbContext class that holds all the repositories in it and initialize them all made the DbContext a component class to enable the dependency injection

--- src/main/java/org/fcai/ordernotificationmodule/OrderModuleApplication.java
+++ src/main/java/org/fcai/ordernotificationmodule/OrderModuleApplication.java
@@ -3,7 +3,7 @@ package org.fcai.ordernotificationmodule;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/")
 public class OrderModuleApplication extends Application {
 
 }
---
+++ src/test/java/com/swe/OrderNotificationModule/OrderNotificationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe.OrderNotificationModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderNotificationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
