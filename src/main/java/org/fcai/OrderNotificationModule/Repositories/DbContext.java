dd736ab24a1fd4725f1a5944f96a05dcdc383efd
Mansour <mansour24@protonmail.com>
2023-12-29 22:30:57 +0200

    Added test cases and another constructor

--- src/main/java/org/fcai/OrderNotificationModule/Repositories/DbContext.java
+++ src/main/java/org/fcai/OrderNotificationModule/Repositories/DbContext.java
@@ -1,5 +1,7 @@
 package org.fcai.OrderNotificationModule.Repositories;
 
+import org.fcai.OrderNotificationModule.Enums.CategoryName;
+import org.fcai.OrderNotificationModule.Models.Category;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,7 +27,20 @@ public class DbContext {
     }
 
     private static CategoryRepository initializeCategoryRepository() {
-        return new CategoryRepository(); // TODO: Implement CategoryRepository Initialization
+        CategoryRepository categoryRepository = new CategoryRepository();
+        categoryRepository.addNewCategory(new Category(1, CategoryName.FOOD, "Soft drinks, coffees, teas, beers, and ales"));
+        categoryRepository.addNewCategory(new Category(2, CategoryName.FURNITURE, "Chairs, tables, beds, desks, dressers, and cupboards"));
+        categoryRepository.addNewCategory(new Category(3, CategoryName.GROCERIES, "Bread, cheese, eggs, milk, and other dairy products"));
+        categoryRepository.addNewCategory(new Category(4, CategoryName.HEALTH, "Medicines, vitamins, bandages, and other medical supplies"));
+        categoryRepository.addNewCategory(new Category(5, CategoryName.HOME, "Cleaning supplies, cooking utensils, and various other household items"));
+        categoryRepository.addNewCategory(new Category(6, CategoryName.KITCHEN, "Pots, pans, dishes, cutlery, and various other kitchen items"));
+        categoryRepository.addNewCategory(new Category(7, CategoryName.OFFICE, "Pens, pencils, paper, binders, and other office supplies"));
+        categoryRepository.addNewCategory(new Category(8, CategoryName.OUTDOORS, "Lawn mowers, shovels, rakes, and other outdoor items"));
+        categoryRepository.addNewCategory(new Category(9, CategoryName.SPORTS, "Balls, bats, gloves, and various other sports items"));
+        categoryRepository.addNewCategory(new Category(10, CategoryName.TOYS, "Dolls, board games, puzzles, and various other toys"));
+        categoryRepository.addNewCategory(new Category(11, CategoryName.OTHER, "Various other items"));
+
+        return categoryRepository;
     }
 
     private static OrderRepository initializeOrderRepository() {
