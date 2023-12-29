8391996cc3428198720470734b18b95ff1ba4da6
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-12-29 21:01:17 +0200

    Update UserRepository.java

--- src/main/java/org/fcai/OrderNotificationModule/Repositories/UserRepository.java
+++ src/main/java/org/fcai/OrderNotificationModule/Repositories/UserRepository.java
@@ -2,12 +2,39 @@ package org.fcai.OrderNotificationModule.Repositories;
 
 import org.fcai.OrderNotificationModule.Models.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository {
+    private final List<User> users;
+
+    public UserRepository() {
+        this.users = new ArrayList<>();
+    }
+
     public User isUserExist(String username, String password) {
-        return null; // TODO: Implement this method.
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
     }
 
     public void registerNewUser(User user) {
-        // TODO: Implement this method.
+        if (isUsernameTaken(user.getUsername())) {
+            throw new IllegalArgumentException("Username is already taken");
+        }
+
+        users.add(user);
+    }
+
+    private boolean isUsernameTaken(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
