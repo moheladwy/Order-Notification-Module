ec1527ab66d337c260d163770ef97b0233e6046c
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-12-30 01:04:14 +0200

    Update DbContext.java

--- src/main/java/org/fcai/OrderNotificationModule/Repositories/DbContext.java
+++ src/main/java/org/fcai/OrderNotificationModule/Repositories/DbContext.java
@@ -1,6 +1,9 @@
 package org.fcai.OrderNotificationModule.Repositories;
 
+import org.fcai.OrderNotificationModule.Models.*;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class DbContext {
@@ -28,7 +31,33 @@ public class DbContext {
         return new CategoryRepository(); // TODO: Implement CategoryRepository Initialization
     }
 
-    private static OrderRepository initializeOrderRepository() {
-        return new OrderRepository(); // TODO: Implement OrderRepository Initialization
-    }
+    public static OrderRepository initializeOrderRepository() {
+            OrderRepository orderRepository = new OrderRepository();
+
+            User user1 = new User("user1", "User One", "user1@example.com", "password1", "Address1", "123456789", "Area1", 1000.0);
+            User user2 = new User("user2", "User Two", "user2@example.com", "password2", "Address2", "987654321", "Area2", 1500.0);
+
+            Product product1 = new Product(0, "Jeans", "Category1", "Brand1", 200.0);
+            Product product2 = new Product(1, "T-shirts", "Category1", "Brand1", 150.0);
+            Product product3 = new Product(2, "Smartphones", "Category2", "Brand2", 1000.0);
+
+            OrderSpecs specs1 = new OrderSpecs(1, user1, 20.0);
+            OrderSpecs specs2 = new OrderSpecs(2, user2, 30.0);
+
+            SimpleOrder simpleOrder1 = new SimpleOrder(specs1);
+            simpleOrder1.addProduct(product1, 2);
+            simpleOrder1.addProduct(product2, 3);
+
+            SimpleOrder simpleOrder2 = new SimpleOrder(specs2);
+            simpleOrder2.addProduct(product3, 1);
+
+            CompoundOrder compoundOrder = new CompoundOrder(specs1, 2, List.of(simpleOrder1, simpleOrder2));
+
+            orderRepository.createSimpleOrder(simpleOrder1);
+            orderRepository.createSimpleOrder(simpleOrder2);
+            orderRepository.createCompoundOrder(compoundOrder);
+
+            return orderRepository;
+        }
+
 }
