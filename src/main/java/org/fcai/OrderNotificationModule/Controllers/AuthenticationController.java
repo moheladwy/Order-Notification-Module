7d06ea4562dc9f8f1b6358efb93f49c3b03be7e6
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-12-30 01:04:31 +0200

    Update AuthenticationController.java

--- src/main/java/org/fcai/OrderNotificationModule/Controllers/AuthenticationController.java
+++ src/main/java/org/fcai/OrderNotificationModule/Controllers/AuthenticationController.java
@@ -3,6 +3,7 @@ package org.fcai.OrderNotificationModule.Controllers;
 import org.fcai.OrderNotificationModule.Helper.LoginRequest;
 import org.fcai.OrderNotificationModule.Models.User;
 import org.fcai.OrderNotificationModule.Repositories.DbContext;
+import org.fcai.OrderNotificationModule.Exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,13 +18,36 @@ public class AuthenticationController {
 
     @PostMapping("/login/")
     public User login(@RequestBody LoginRequest loginRequest) {
+
+        if(loginRequest == null) {
+            throw new IllegalArgumentException("Login request cannot be null");
+        }
+
         String username = loginRequest.getUsername();
         String password = loginRequest.getPassword();
-        return context.userRepository.isUserExist(username, password);
+
+        // Check for null values
+        if (username == null || password == null) {
+            throw new IllegalArgumentException("Username and password cannot be null");
+        }
+
+        User user = context.userRepository.isUserExist(username, password);
+
+        // Throw exception if user is not found
+        if (user == null) {
+            throw new UserNotFoundException(username);
+        }
+
+        return user;
     }
 
     @PostMapping("/register/")
     public void register(@RequestBody User user) {
+        // Check for null values
+        if (user == null || user.getUsername() == null || user.getPassword() == null) {
+            throw new IllegalArgumentException("username, and password cannot be null");
+        }
+
         context.userRepository.registerNewUser(user);
     }
 
