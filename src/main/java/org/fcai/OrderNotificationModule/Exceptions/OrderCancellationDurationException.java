e18024f4c6e399384ea425a676c6a6b07d3d613d
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:52:59 +0200

    Delete OrderCancellationDurationException.java

--- src/main/java/org/fcai/OrderNotificationModule/Exceptions/OrderCancellationDurationException.java
+++
@@ -1,7 +0,0 @@
-package org.fcai.OrderNotificationModule.Exceptions;
-
-public class OrderCancellationDurationException extends RuntimeException {
-    public OrderCancellationDurationException(String message) {
-        super(message);
-    }
-}
