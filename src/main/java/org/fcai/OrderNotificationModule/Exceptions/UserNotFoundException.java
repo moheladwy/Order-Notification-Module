1abdc8758c246ce59cd548a5326de05ac6519b94
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:51:54 +0200

    Delete UserNotFoundException.java

--- src/main/java/org/fcai/OrderNotificationModule/Exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.fcai.OrderNotificationModule.Exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String username) {
-        super("User with username " + username + " not found.");
-    }
-}
