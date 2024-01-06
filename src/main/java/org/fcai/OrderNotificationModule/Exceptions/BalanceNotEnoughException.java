cd82993ca685321fece4e81d04a4d02bb730d28c
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:53:03 +0200

    Delete BalanceNotEnoughException.java

--- src/main/java/org/fcai/OrderNotificationModule/Exceptions/BalanceNotEnoughException.java
+++
@@ -1,9 +0,0 @@
-package org.fcai.OrderNotificationModule.Exceptions;
-
-import org.fcai.OrderNotificationModule.Models.User;
-
-public class BalanceNotEnoughException extends RuntimeException{
-    public BalanceNotEnoughException(User user, double price) {
-        super("Not enough balance for user: " + user.getUsername() + " to buy product with price " + price + ". Current balance is " + user.getBalance() + ".");
-    }
-}
