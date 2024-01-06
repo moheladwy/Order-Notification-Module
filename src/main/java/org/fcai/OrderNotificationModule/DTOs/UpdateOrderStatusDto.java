359157daebe03fd481b1191734ac532bc792f35a
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:53:17 +0200

    Delete UpdateOrderStatusDto.java

--- src/main/java/org/fcai/OrderNotificationModule/DTOs/UpdateOrderStatusDto.java
+++
@@ -1,8 +0,0 @@
-package org.fcai.OrderNotificationModule.DTOs;
-
-import org.fcai.OrderNotificationModule.Enums.OrderStatus;
-
-public class UpdateOrderStatusDto {
-    public int orderId;
-    public OrderStatus status;
-}
