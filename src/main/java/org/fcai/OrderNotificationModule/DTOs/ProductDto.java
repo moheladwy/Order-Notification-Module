49a7b41fe4dd7f23875e2e9abf2c35e5ece681af
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:53:22 +0200

    Delete ProductDto.java

--- src/main/java/org/fcai/OrderNotificationModule/DTOs/ProductDto.java
+++
@@ -1,9 +0,0 @@
-package org.fcai.OrderNotificationModule.DTOs;
-
-public class ProductDto {
-    public String name;
-    public String description;
-    public String vendor;
-    public double price;
-    public int quantity;
-}
