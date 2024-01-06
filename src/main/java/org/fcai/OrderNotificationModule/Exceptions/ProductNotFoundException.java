ba2c219430ce9b244be2515858a1491bd1328a43
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:52:49 +0200

    Delete ProductNotFoundException.java

--- src/main/java/org/fcai/OrderNotificationModule/Exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.fcai.OrderNotificationModule.Exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(int id) {
-        super("Product with id " + id + " does not exist.");
-    }
-}
