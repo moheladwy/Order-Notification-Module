fa4b1fc6283da27dcd56e4fed863f88cf439c67f
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:51:14 +0200

    Create ProductDto.java

---
+++ Order-Notification-Module/src/main/java/org/fcai/OrderNotificationModule/DTOs/ProductDto.java
@@ -0,0 +1,9 @@
+package org.fcai.OrderNotificationModule.DTOs;
+
+public class ProductDto {
+    public String name;
+    public String description;
+    public String vendor;
+    public double price;
+    public int quantity;
+}
