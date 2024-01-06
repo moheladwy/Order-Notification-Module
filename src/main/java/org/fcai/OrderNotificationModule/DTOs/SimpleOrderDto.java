2754ff07044189b36fd4eb51f6167c41e907b357
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:53:19 +0200

    Delete SimpleOrderDto.java

--- src/main/java/org/fcai/OrderNotificationModule/DTOs/SimpleOrderDto.java
+++
@@ -1,9 +0,0 @@
-package org.fcai.OrderNotificationModule.DTOs;
-
-import java.util.List;
-
-public class SimpleOrderDto {
-    public double shippingFees;
-    public String username;
-    public List<List<Integer>> products;
-}
