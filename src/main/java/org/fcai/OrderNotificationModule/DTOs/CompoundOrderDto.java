93d4a93b8cfa9b00e7c1bde6b029e02412304476
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-01-06 23:51:50 +0200

    Delete CompoundOrderDto.java

--- src/main/java/org/fcai/OrderNotificationModule/DTOs/CompoundOrderDto.java
+++
@@ -1,9 +0,0 @@
-package org.fcai.OrderNotificationModule.DTOs;
-
-import java.util.List;
-
-public class CompoundOrderDto {
-    public List<SimpleOrderDto> simpleOrders;
-    public double shippingFees;
-    public String usernameOfSuperUser;
-}
